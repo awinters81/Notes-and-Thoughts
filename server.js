df4ce14d4a04ef63acb7057a899721f540132c5c
awinters81 <alexandria.winters81@gmail.com>
2023-01-17 19:09:57 -0500

    adding db.json

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // Because I already know I need Express.js, I know I will need to create a server.js file.
 
 const express = require('express');
-const apiRoute = require('./routes/apiRoute');
-const htmlRoute = require('./routes/htmlRoute');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/api', apiRoute);
-app.use('/', htmlRoute);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
